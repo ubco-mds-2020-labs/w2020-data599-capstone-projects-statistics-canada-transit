db0726f5e37151f9eb9f0c242401953f636bd79a
Yuxuan Cui <cuiyuuxuan@gmail.com>
2021-05-28 14:44:54 -0700

    using Rain suggested formula

--- code/Score Modeling/Score_Functions.R
+++ code/Score Modeling/Score_Functions.R
@@ -67,6 +67,46 @@ sum_score_fxn <- function(df, weight = FALSE, log_normalize_score = TRUE, normal
   df
 }
 
+
+
+# sum score function : SUM [i..n] (1 / (traveltime_i + std_traveltime_i) + ... ))
+
+sum_score_fxn_2 <- function(df, weight = FALSE, log_normalize_score = TRUE, normalize_df = FALSE, x=1, y=10) {
+  
+  
+  # custom range data normalization prior to score computation
+  # log is false since we don't care about correcting skew at this point
+  if (normalize_df == TRUE) { df <- normalize_df(df, x = x, y = y, log = FALSE) }
+  
+  # compute score for each row
+  if (weight == FALSE) { df$unique_score <- 1/(df$avg_time+2*df$sd_time) } 
+  else { df$unique_score <- df$weight/(df$avg_time+2*df$sd_time) }
+  
+  ## sum scores and normalize (OPTION 1)
+  df <- df %>% 
+    group_by(fromId, type) %>% 
+    summarise(score = sum(unique_score)) %>%
+    group_by(type) %>%
+    mutate(score = normalize_vec(score, x = 0.01, y = 0.99, log = log_normalize_score))
+  
+  ## sum scores and normalize (OPTION 2) - less efficient
+  #df <- df %>% 
+  #    group_by(fromId, type) %>%
+  #    summarise(score = sum(unique_score))
+  
+  # split df by amenity type
+  #df_list <- split(df, df$type)
+  
+  # normalize each df
+  #df_list <- lapply(df_list, normalize_df, x = 0.01, y = 0.99, log = log_normalize_score)
+  
+  # recombine
+  #df <- data.table::rbindlist(df_list) %>% arrange(fromId)
+  
+  df
+}
+
+
 # OLD AND NO LONGER USED TO COMPUTE SCORES 
 # naive score function : accessible_points / (mean * std)
 
